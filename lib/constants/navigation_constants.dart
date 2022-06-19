4d6816ca331fb6902f7bc5510c9a95fefe1a913b
Patrick Witter <pwitterop123@gmail.com>
2022-06-19 15:12:36 -0500

    Completed navigation constants and routing logic

--- lib/constants/navigation_constants.dart
+++ lib/constants/navigation_constants.dart
@@ -1,3 +1,5 @@
 class Routes {
   static const String home = "/home";
+  static const String signin = "/signin";
+  static const String chatscreen = "/chatscreen";
 }
--- lib/route.dart
+++ lib/route.dart
@@ -1,12 +1,24 @@
 import 'package:flutter/material.dart';
 import 'package:messengerapp/constants/navigation_constants.dart';
+import 'package:messengerapp/views/chatscreen.dart';
 import 'package:messengerapp/views/defaultpage.dart';
 import 'package:messengerapp/views/home.dart';
+import 'package:messengerapp/views/signin.dart';
 
 Route<dynamic> ongenerateRoute(RouteSettings settings) {
   switch (settings.name) {
     case Routes.home:
       return MaterialPageRoute(builder: (context) => const Home());
+    case Routes.signin:
+      return MaterialPageRoute(builder: ((context) => const SignIn()));
+    case Routes.chatscreen:
+      Map<String, dynamic> args = settings.arguments as Map<String, dynamic>;
+      return MaterialPageRoute(
+        builder: ((context) => ChatScreen(
+              chatWithUsername: args["name"],
+              name: args["chatUser"],
+            )),
+      );
     default:
       return MaterialPageRoute(builder: (context) => const DefaultPage());
   }
